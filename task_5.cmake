2dc7c0027455dd4101a8ac96fedd41ef53d1f098
Frontier789 <fr0nt13r789@gmail.com>
2024-12-14 12:47:59 +0100

    Task 5 done for real

--- task_5.cmake
+++ task_5.cmake
@@ -16,6 +16,10 @@ string(REPLACE "\n" "|" THE_PAGE_RULE "${PAGE_RULES_REGEX}")
 # message(TESTS_LIST="${TESTS_LIST}")
 # message(THE_PAGE_RULE="${THE_PAGE_RULE}")
 
+# #
+# Task 1
+# #
+
 SET(SUM_OF_MIDDLES 0)
 
 foreach(CASE ${TESTS_LIST})
@@ -37,3 +41,42 @@ foreach(CASE ${TESTS_LIST})
 endforeach()
 
 message("Task 1: ${SUM_OF_MIDDLES}")
+
+# #
+# Task 2
+# #
+
+string(REPLACE "\n" ";" PAGE_RULES_LIST "${PAGE_RULES}")
+
+# message(PAGE_RULES_LIST="${PAGE_RULES_LIST}")
+
+SET(SUM_OF_MIDDLES 0)
+
+foreach(CASE ${TESTS_LIST})
+    SET(UPDATED "${CASE}")
+    
+    while(1)
+        SET(START_LIST "${UPDATED}")
+        foreach(RULE ${PAGE_RULES_LIST})
+            string(REGEX REPLACE "([0-9][0-9])\\|([0-9][0-9])" "(.*)(\\2)(.*)(\\1)(.*)" RULE_VIOLATION_FINDER "${RULE}")
+            
+            # message("UPDATED=${UPDATED} RULE_VIOLATION_FINDER=${RULE_VIOLATION_FINDER}")
+            string(REGEX REPLACE "${RULE_VIOLATION_FINDER}" "\\1\\4\\3\\2\\5" UPDATED "${UPDATED}")
+        endforeach()
+        # message("")
+        if("${START_LIST} " MATCHES "${UPDATED} ")
+            break()
+        endif()
+    endwhile()
+    
+    if(NOT "${CASE} " MATCHES "${UPDATED} ")
+        # message("${CASE} -> ${UPDATED}")
+        string(REPLACE "," ";" NUMS "${UPDATED}")
+        list(LENGTH NUMS NUM_COUNT)
+        math(EXPR MIDDLE_INDEX "(${NUM_COUNT}-1)/2")
+        list(GET NUMS "${MIDDLE_INDEX}" MIDDLE_VALUE)
+        math(EXPR SUM_OF_MIDDLES "${SUM_OF_MIDDLES} + ${MIDDLE_VALUE}")
+    endif()
+endforeach()
+
+message("Task 2: ${SUM_OF_MIDDLES}")
